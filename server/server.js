ad4a8fc97c7b9a3a894b0c9cdd6a391a0bfa5b53
Alex Benko <benkoalex521@gmail.com>
2020-04-19 15:10:23 -0700

    Removed un needed code

--- server/server.js
+++ server/server.js
@@ -9,14 +9,15 @@ const port = 4200;
 app.use(cors());
 app.use(bodyParser.json()); //makes sure the body is parsed into a JSON
 
-app.get('/test', (req,res) => res.send('Set up Correctly')); //want to make sure the api is set up
+//app.get('/test', (req,res) => res.send('Set up Correctly')); want to make sure the api is set up
 
+//this send the file entire app to the server
 app.use('/', express.static(path.join(__dirname, '../dist')));
 
-app.get('/lolstats_backend', (req,res)=>{
+/*app.get('/lolstats_backend', (req,res)=>{
   res.send({express: "Connected to Backend!"});
 });
-
+*/
 
 app.listen(port, () =>{
   console.log(`lolstats is runnin at port : ${port}`);
