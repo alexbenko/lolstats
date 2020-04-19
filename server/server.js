4d9baf43928f1c45ebb2a15e1f9ebb405c610e58
Alex Benko <benkoalex521@gmail.com>
2020-04-19 13:58:01 -0700

    Set up basic server and successfully loads the webpage

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const port = 4200;
 
-app.listen(port, () =>{
-  console.log(`lolstats is runnin at port : ${port}`);
-})
+app.use(cors());
+app.use(bodyParser.json()); //makes sure the body is parsed into a JSON
+
+app.get('/test', (req,res) => res.send('Set up Correctly')); //want to make sure the api is set up
+
+app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.get('/lolstats_backend', (req,res)=>{
   res.send({express: "Connected to Backend!"});
-})
+});
+
+
+app.listen(port, () =>{
+  console.log(`lolstats is runnin at port : ${port}`);
+});
+
