6d4e9d3d43a950a1c32eff891e016f3b2672c51f
Alex Benko <benkoalex521@gmail.com>
2020-04-22 22:06:14 -0700

    Get request for rank data

---
+++ client/components/getRank.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+var getRank = ({encryptedId,key},cb) =>{
+  let proxy = 'https://cors-anywhere.herokuapp.com/';
+  let target = `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedId}?api_key=${key}`;
+
+  console.log("Getting Rank");
+  axios.get(proxy + target)
+  .then((res) =>{
+    console.log("Resuts from Ranl: ",res)
+    cb(res.data);
+  })
+  .catch((err)=>{
+    console.error("Error:",err);
+
+  })
+};
+
+export default getRank;
