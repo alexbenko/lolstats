c6c7a7ece29f108a2da1204c29162f128423a796
Alex Benko <benkoalex521@gmail.com>
2020-04-22 22:08:34 -0700

    Added rank info and win info

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 var Profile = (props) => (
 
-<div>
   <div className="prof">
+     {console.log(props.rank[0].tier)}
     <h3>{`Summoner: ${props.profile.name}`}</h3>
     <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
+    <h3>{`Solo-Duo Rank: ${props.rank[0].tier + ' ' + props.rank[0].rank}`}</h3>
+    <h3>{`Total Wins This Season: ${props.rank[0].wins}`}</h3>
   </div>
-</div>
+
 );
 
 
