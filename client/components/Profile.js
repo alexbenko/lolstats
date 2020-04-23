713a33bd08c6d52877f555b1289d25bde887df68
Alex Benko <benkoalex521@gmail.com>
2020-04-23 09:06:18 -0700

    FIxed so it properly displays solo duo rank

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -3,11 +3,11 @@ import React from 'react';
 var Profile = (props) => (
 
   <div className="prof">
-     {console.log(props.rank[0].tier)}
+     {console.log("From profile: ",props.rank)}
     <h3>{`Summoner: ${props.profile.name}`}</h3>
     <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
-    <h3>{`Solo-Duo Rank: ${props.rank[0].tier + ' ' + props.rank[0].rank}`}</h3>
-    <h3>{`Total Wins This Season: ${props.rank[0].wins}`}</h3>
+    <h3>{`Solo-Duo Rank: ${props.rank[1].tier + ' ' + props.rank[1].rank}`}</h3>
+    <h3>{`Total Wins This Season: ${props.rank[1].wins}`}</h3>
   </div>
 
 );
