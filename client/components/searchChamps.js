d7adbd60de2f398d5cd70672b60d4838194b6203
Alex Benko <benkoalex521@gmail.com>
2020-04-22 22:09:11 -0700

    Removed Comments

--- client/components/searchChamps.js
+++ client/components/searchChamps.js
@@ -4,12 +4,9 @@ var searchForChampions = ({encryptedId,key}, cb) =>{
   //change the state inside the get request
   var proxy = 'https://cors-anywhere.herokuapp.com/';
   var target = `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${encryptedId}?api_key=${key}`;
-  console.log("searchForChamps Id:" ,encryptedId);
-
 
   axios.get(proxy + target)
     .then(res => {
-      console.log("Data from searchChamps: ",res.data);
       cb(res.data);
     })
     .catch((error) => {
