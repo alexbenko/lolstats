4baa599162ba75d31a43015eca0c3d81ffba1153
Alex Benko <benkoalex521@gmail.com>
2020-04-24 21:50:12 -0700

    Fixed so it correctly displays rank data

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -6,8 +6,8 @@ var Profile = (props) => (
      {console.log("From profile: ",props.rank)}
     <h3>{`Summoner: ${props.profile.name}`}</h3>
     <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
-    <h3>{`Solo-Duo Rank: ${props.rank[1].tier + ' ' + props.rank[1].rank}`}</h3>
-    <h3>{`Total Wins This Season: ${props.rank[1].wins}`}</h3>
+    <h3>{`Solo-Duo Rank: ${props.rank[0].tier + ' ' + props.rank[0].rank}`}</h3>
+    <h3>{`Total Wins This Season: ${props.rank[0].wins}`}</h3>
   </div>
 
 );
