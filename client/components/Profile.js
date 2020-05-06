a7c7874c0f913a692a1fcbadbecb427aaeb5889d
Alex Benko <benkoalex521@gmail.com>
2020-05-05 20:13:56 -0700

    Now displays emblem for rank

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react';
+var capitalizeFirstLetter = (string) =>{
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 var Profile = (props) => (
 
   <div className="prof">
     <h3>{`Summoner: ${props.profile.name}`}</h3>
-
     <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
+    <img src={`https://rankedimages.s3-us-west-2.amazonaws.com/Emblem_${capitalizeFirstLetter((props.rank[0].tier).toLowerCase())}.png`} className='rank'/>
     <h3>{`Solo-Duo Rank: ${props.rank[0].tier + ' ' + props.rank[0].rank}`}</h3>
     <h3>{`Total Wins This Season: ${props.rank[0].wins}`}</h3>
   </div>
