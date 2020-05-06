172067602c4a84022804790d20f742f19f7fabbc
Alex Benko <benkoalex521@gmail.com>
2020-05-05 17:24:44 -0700

    Push

--- client/index.js
+++ client/index.js
@@ -9,9 +9,7 @@ import getRank from './components/getRank.js';
 
 
 //AWS//
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+
 //AWS//
 
 
