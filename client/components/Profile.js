5cf8dba8b2b83d52914c04daa3dcff611f95cad5
Alex Benko <benkoalex521@gmail.com>
2020-05-05 20:19:27 -0700

    Now displays profiles summoner icon

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -7,6 +7,7 @@ var Profile = (props) => (
 
   <div className="prof">
     <h3>{`Summoner: ${props.profile.name}`}</h3>
+    <img src={`http://ddragon.leagueoflegends.com/cdn/10.9.1/img/profileicon/${props.profile.profileIconId}.png`} className='rank'/>
     <h3>{`Level: ${props.profile.summonerLevel}`}</h3>
     <img src={`https://rankedimages.s3-us-west-2.amazonaws.com/Emblem_${capitalizeFirstLetter((props.rank[0].tier).toLowerCase())}.png`} className='rank'/>
     <h3>{`Solo-Duo Rank: ${props.rank[0].tier + ' ' + props.rank[0].rank}`}</h3>
